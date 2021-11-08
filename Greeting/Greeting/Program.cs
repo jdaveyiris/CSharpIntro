3a5be39a642b9ccecef493d5fcc7533798a813a4
Jim Davey <Jim.Davey@IRIS.CO.UK>
2021-11-08 14:04:17 +0000

    My first piece of C# code

---
+++ Greeting/Greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string aFriend = Console.ReadLine();
+            Console.WriteLine($"Hello, {aFriend} Nice to meet you ");
+        }
+    }
+}
