3d56537bcaf1a2a128975d9b2f76e1fad31c852d
Jim Davey <Jim.Davey@IRIS.CO.UK>
2021-11-09 12:13:01 +0000

    Branching

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,13 +6,38 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter an operator from the following: +, -, * or /: ");
+            string operation = Console.ReadLine();      
+            //int operation = int.Parse(Console.ReadLine()); (Thought this may allow for the input to be used as shown on line 21
+            double solution = 0;
+
             Console.Write("What is your first number? ");
             double firstNumber = Double.Parse(Console.ReadLine());
 
             Console.Write("What is your first number? ");
             double secondNumber = Double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Together they multiply to make: " + (firstNumber * secondNumber));
+            // Can't simply force the input for operator into a one line solution.
+            //sum = (firstNumber)(operation)(secondNumber);
+
+            if (operation == "+")
+            {
+                solution = firstNumber + secondNumber;
+            }
+            else if (operation == "-")
+            {
+                solution = firstNumber - secondNumber;
+            }
+            else if (operation == "*")
+            {
+                solution = firstNumber * secondNumber;
+            }
+            else
+            {
+                solution = firstNumber / secondNumber;
+            }
+
+            Console.WriteLine("The answer is " + solution);
             Console.ReadLine();
         }
     }
--- Greeting/Greeting/Program.cs
+++ Greeting/Greeting/Program.cs
@@ -19,7 +19,8 @@ namespace Greeting
             Console.WriteLine(y);
             Console.ReadLine();
             
-
+            ---------
+            
             Console.WriteLine("What is your name?");
             Console.Write("Type your first name:");
             string myFirstName;
@@ -29,9 +30,24 @@ namespace Greeting
             Console.Write("Type your last name:");
             string myLastName = Console.ReadLine();
 
-            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            string message = "";
+
+            if ((myFirstName == "") && (myLastName == ""))
+            {
+                message = "You really ought to tell me your name you know.";
+            }
+            else if ((myFirstName == "") || (myLastName == ""))
+            {
+                message = ("Hello," + myFirstName + myLastName + "! I see you only have one name, but that is fine");
+            }
+            else
+            {
+                message = "Hello, " + myFirstName + " " + myLastName;
+            }
+            Console.WriteLine(message);
             Console.ReadLine();
-             */
+
+            ----------
 
             Console.Write("What is the circles radius? ");
             string inputRadius = Console.ReadLine();
@@ -41,6 +57,49 @@ namespace Greeting
             double areaCircle = Math.PI * (radius * radius);
             Console.WriteLine(areaCircle);
             Console.ReadLine();
+            
+            ------------
+
+            Console.WriteLine("Jim's Mad Prizes");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+            string message = "";
+
+            if (userValue == "1")
+            {
+                message = "You won a fur coat!";
+                //Console.WriteLine(message);
+            }
+            else if ( userValue == "2")
+                message = "You won an Xbox!";         
+            else if (userValue == "3")
+            {
+                message = "You won a car!";            
+            }
+            else
+            {
+                message = "Sorry that wasn't what you were meant to do! ";
+                message += "Womp Womp.";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+          
+
+            Console.WriteLine("Jim's Mad Prizes");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            Console.Write("You won a {0}.", message);
+            Console.ReadLine();
+
+            ---------
+
+            */
+
+
 
         }
     }
